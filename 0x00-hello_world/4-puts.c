7d36a8d6001527986dabd57c72206be8805b8942
xwilis <xhawilis@gmail.com>
2023-03-18 12:53:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
 */
 
-int main(void) {
-  
-  puts ("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
   return (0);
 }
