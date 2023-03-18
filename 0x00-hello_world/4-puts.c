edbdaf6162c36398a5587375bec7a247cf0b47d1
xwilis <xhawilis@gmail.com>
2023-03-18 04:44:24 -0700

    0x00 Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{  
+    puts("programming is like building a multilingualpuzzle\n");
+
+    return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <stdio.h>
+
+void int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return 0;
+}
