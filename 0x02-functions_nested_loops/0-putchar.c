032d8a60d05a9aa22cc577a952d138902d099503
xwilis <xhawilis@gmail.com>
2023-03-22 12:52:16 -0700

    ouch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,23 +8,23 @@
  */
 int main(void)
 {
-  	char letter;
-  	char ns = '_';
-  	putchar (ns);
-  	letter = 'p';
-  	putchar (letter);
-  	letter = 'u';
-  	putchar (letter);
-  	letter = 't';
-  	putchar (letter);
-  	letter = 'c';
-	putchar (letter);
-	letter = 'h';
-	putchar (letter);
-	letter = 'a';
-	putchar (letter);
-	letter = 'r';
-	putchar(letter);
-	putchar('\n');
-	return (0);
+char letter;
+char ns = '_';
+putchar (ns);
+letter = 'p';
+putchar (letter);
+letter = 'u';
+putchar (letter);
+letter = 't';
+putchar (letter);
+letter = 'c';
+putchar (letter);
+letter = 'h';
+putchar (letter);
+letter = 'a';
+putchar (letter);
+letter = 'r';
+putchar(letter);
+putchar('\n');
+return (0);
 }
